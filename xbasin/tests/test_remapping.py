ce136a78e07ac8ac2d04b6417c118e6e69ff9372
Romain <romain.caneill@yahoo.fr>
2020-04-29 19:25:49 +0200

    reshaped the tests with more stable dataset

--- xbasin/tests/test_remapping.py
+++ xbasin/tests/test_remapping.py
@@ -12,15 +12,16 @@ TESTPATH = Path(os.path.dirname(os.path.abspath(__file__)))
 
 
 _error = 1e-10
-_metrics = {
-    ("X",): ["e1t", "e1u", "e1v", "e1f"],  # X distances
-    ("Y",): ["e2t", "e2u", "e2v", "e2f"],  # Y distances
-    ("Z",): ["e3t_0", "e3u_0", "e3v_0", "e3f_0", "e3w_0"],  # Z distances
+
+metrics_fr = {
+    ("X",): ["e1t", "e1u", "e1v", "e1f"],
+    ("Y",): ["e2t", "e2u", "e2v", "e2f"],
+    ("Z",): ["e3t", "e3u", "e3v", "e3w"],
 }
-_metrics_to = {
-    ("X",): ["e1t", "e1u", "e1v", "e1f"],  # X distances
-    ("Y",): ["e2t", "e2u", "e2v", "e2f"],  # Y distances
-    ("Z",): ["e3t_1d", "e3w_1d"],  # Z distances
+metrics_to = {
+    ("X",): ["e1t", "e1u", "e1v", "e1f"],
+    ("Y",): ["e2t", "e2u", "e2v", "e2f"],
+    ("Z",): ["e3t_1d", "e3w_1d"],
 }
 
 
@@ -63,31 +64,26 @@ def _assert_same_integrated_value(v_fr, v_to, e3_fr, e3_to, error=_error):
         assert (int_to - int_fr < error).all()
 
 
-def open_domcfg_to():
-    domcfg_to = xr.open_dataset(TESTPATH / "data/xnemogcm.domcfg_to.nc")
-    return domcfg_to
-
-
-def open_domcfg_fr():
-    domcfg_fr = xr.open_dataset(TESTPATH / "data/xnemogcm.domcfg_fr.nc")
-    domcfg_fr.load()
+def open_ds():
+    ds = xr.open_dataset(TESTPATH / "data/nemo_full_dataset.nc")
+    ds.load()
     # correct error in domcfg
-    domcfg_fr["gdept_0"] = domcfg_fr["gdept_0"].transpose("x_c", "y_c", "z_c")
-    domcfg_fr["gdept_0"][0] = domcfg_fr["gdept_0"][1]
-    domcfg_fr["gdept_0"][-1] = domcfg_fr["gdept_0"][-2]
-    domcfg_fr["gdept_0"][:, 0] = domcfg_fr["gdept_0"][:, 1]
-    domcfg_fr["gdept_0"][:, -1] = domcfg_fr["gdept_0"][:, -2]
-    domcfg_fr["gdepw_0"] = domcfg_fr["gdepw_0"].transpose("x_c", "y_c", "z_f")
-    domcfg_fr["gdepw_0"][0] = domcfg_fr["gdepw_0"][1]
-    domcfg_fr["gdepw_0"][-1] = domcfg_fr["gdepw_0"][-2]
-    domcfg_fr["gdepw_0"][:, 0] = domcfg_fr["gdepw_0"][:, 1]
-    domcfg_fr["gdepw_0"][:, -1] = domcfg_fr["gdepw_0"][:, -2]
-    domcfg_fr["e3w_0"] = domcfg_fr["e3w_0"].transpose("x_c", "y_c", "z_f")
-    domcfg_fr["e3w_0"][0] = domcfg_fr["e3w_0"][1]
-    domcfg_fr["e3w_0"][-1] = domcfg_fr["e3w_0"][-2]
-    domcfg_fr["e3w_0"][:, 0] = domcfg_fr["e3w_0"][:, 1]
-    domcfg_fr["e3w_0"][:, -1] = domcfg_fr["e3w_0"][:, -2]
-    return domcfg_fr
+    ds["gdept_0"] = ds["gdept_0"].transpose("x_c", "y_c", "z_c")
+    ds["gdept_0"][0] = ds["gdept_0"][1]
+    ds["gdept_0"][-1] = ds["gdept_0"][-2]
+    ds["gdept_0"][:, 0] = ds["gdept_0"][:, 1]
+    ds["gdept_0"][:, -1] = ds["gdept_0"][:, -2]
+    ds["gdepw_0"] = ds["gdepw_0"].transpose("x_c", "y_c", "z_f")
+    ds["gdepw_0"][0] = ds["gdepw_0"][1]
+    ds["gdepw_0"][-1] = ds["gdepw_0"][-2]
+    ds["gdepw_0"][:, 0] = ds["gdepw_0"][:, 1]
+    ds["gdepw_0"][:, -1] = ds["gdepw_0"][:, -2]
+    ds["e3w_0"] = ds["e3w_0"].transpose("x_c", "y_c", "z_f")
+    ds["e3w_0"][0] = ds["e3w_0"][1]
+    ds["e3w_0"][-1] = ds["e3w_0"][-2]
+    ds["e3w_0"][:, 0] = ds["e3w_0"][:, 1]
+    ds["e3w_0"][:, -1] = ds["e3w_0"][:, -2]
+    return ds
 
 
 def test_reshaping():
@@ -100,338 +96,244 @@ def test_reshaping():
 
 
 def test_T_0_same_fr_and_to():
-    domcfg_fr = open_domcfg_fr()
+    ds = open_ds()
 
-    nemo_ds = xr.open_dataset(TESTPATH / "data/xnemogcm.nemo.nc")
-    nemo_ds.load()
-    domcfg_to = domcfg_fr
+    grid_fr = xgcm.Grid(ds, periodic=False)
+    grid_to = xgcm.Grid(ds, periodic=False)
 
-    grid_fr = xgcm.Grid(domcfg_fr, periodic=False)
-    grid_to = xgcm.Grid(domcfg_to, periodic=False)
-
-    v_fr = nemo_ds["thetao"] * 0 * domcfg_fr.tmask
+    v_fr = ds["thetao"] * 0 * ds.tmask
     v_to = remap_vertical(
         v_fr,
         grid_fr,
         grid_to,
         axis="Z",
-        scale_factor_fr=domcfg_fr.e3t_0,
-        scale_factor_to=domcfg_to.e3t_0,
+        scale_factor_fr=ds.e3t,
+        scale_factor_to=ds.e3t_1d,
     )
     _assert_same_domcfg(v_fr, v_to)
 
 
 def test_W_0_same_fr_and_to():
-    domcfg_fr = open_domcfg_fr()
+    ds = open_ds()
 
-    nemo_ds = xr.open_dataset(TESTPATH / "data/xnemogcm.nemo.nc")
-    nemo_ds.load()
-    domcfg_to = domcfg_fr
+    grid_fr = xgcm.Grid(ds, periodic=False)
+    grid_to = xgcm.Grid(ds, periodic=False)
 
-    grid_fr = xgcm.Grid(domcfg_fr, periodic=False)
-    grid_to = xgcm.Grid(domcfg_to, periodic=False)
-
-    v_fr = nemo_ds["woce"] * 0
+    v_fr = ds["woce"] * 0
     v_to = remap_vertical(
         v_fr,
         grid_fr,
         grid_to,
         axis="Z",
-        scale_factor_fr=domcfg_fr.e3w_0,
-        scale_factor_to=domcfg_to.e3w_0,
+        scale_factor_fr=ds.e3w,
+        scale_factor_to=ds.e3w_1d,
     )
     _assert_same_domcfg(v_fr, v_to)
 
 
 def test_W_1_same_fr_and_to():
-    domcfg_fr = open_domcfg_fr()
+    ds = open_ds()
 
-    nemo_ds = xr.open_dataset(TESTPATH / "data/xnemogcm.nemo.nc")
-    nemo_ds.load()
-    domcfg_to = open_domcfg_to()
+    grid_fr = xgcm.Grid(ds, periodic=False, metrics=metrics_fr)
+    grid_to = xgcm.Grid(ds, periodic=False, metrics=metrics_to)
 
-    grid_fr = xgcm.Grid(domcfg_fr, periodic=False, metrics=_metrics)
-    grid_to = xgcm.Grid(domcfg_to, periodic=False, metrics=_metrics_to)
-
-    v_fr = nemo_ds["woce"] * 0 + 1
+    v_fr = ds["woce"] * 0 + 1
     v_to = remap_vertical(v_fr, grid_fr, grid_to, axis="Z",)
     _assert_same_domcfg(v_fr, v_to)
 
 
 def test_T_0():
-    domcfg_fr = open_domcfg_fr()
-    nemo_ds = xr.open_dataset(TESTPATH / "data/xnemogcm.nemo.nc")
-    nemo_ds.load()
-    domcfg_to = open_domcfg_to()
+    ds = open_ds()
 
-    grid_fr = xgcm.Grid(domcfg_fr, periodic=False)
-    grid_to = xgcm.Grid(domcfg_to, periodic=False)
+    grid_fr = xgcm.Grid(ds, periodic=False)
+    grid_to = xgcm.Grid(ds, periodic=False)
 
-    v_fr = nemo_ds["thetao"] * 0 * domcfg_fr.tmask
+    v_fr = ds["thetao"] * 0 * ds.tmask
     v_to = remap_vertical(
         v_fr,
         grid_fr,
         grid_to,
         axis="Z",
-        scale_factor_fr=domcfg_fr.e3t_0,
-        scale_factor_to=domcfg_to.e3t_0,
+        scale_factor_fr=ds.e3t,
+        scale_factor_to=ds.e3t_1d,
     )
-    _assert_same_integrated_value(
-        v_fr, v_to, e3_fr=domcfg_fr.e3t_0, e3_to=domcfg_to.e3t_0
-    )
-
-
-def create_depth_from_e3(domcfg, grid, point="T"):
-    depthw = grid.cumsum(domcfg.e3t_0, axis="Z", boundary="fill", fill_value=0)
-    depthuw = grid.cumsum(domcfg.e3u_0, axis="Z", boundary="fill", fill_value=0)
-    depthvw = grid.cumsum(domcfg.e3v_0, axis="Z", boundary="fill", fill_value=0)
-    depthfw = grid.cumsum(domcfg.e3f_0, axis="Z", boundary="fill", fill_value=0)
-    #
-    deptht = grid.cumsum(
-        domcfg.e3w_0, axis="Z", boundary="fill", fill_value=0
-    ) - 0.5 * domcfg.e3w_0.isel({"z_f": 0})
-    print(deptht)
-    print(np.abs((deptht - domcfg.gdept_0)[:, 1:-1, 1:-1]).max())
+    _assert_same_integrated_value(v_fr, v_to, e3_fr=ds.e3t, e3_to=ds.e3t_1d)
 
 
 def test_U_0():
-    domcfg_fr = open_domcfg_fr()
-    nemo_ds = xr.open_dataset(TESTPATH / "data/xnemogcm.nemo.nc")
-    nemo_ds.load()
-    domcfg_to = open_domcfg_to()
+    ds = open_ds()
 
-    grid_fr = xgcm.Grid(domcfg_fr, periodic=False)
-    grid_to = xgcm.Grid(domcfg_to, periodic=False)
+    grid_fr = xgcm.Grid(ds, periodic=False)
+    grid_to = xgcm.Grid(ds, periodic=False)
 
-    v_fr = nemo_ds["uo"] * 0
+    v_fr = ds["uo"] * 0
     v_to = remap_vertical(
         v_fr,
         grid_fr,
         grid_to,
         axis="Z",
-        scale_factor_fr=domcfg_fr.e3u_0,
-        scale_factor_to=domcfg_to.e3u_0,
-        z_fr=grid_fr.interp(domcfg_fr.gdepw_0, "X", boundary="extend"),
-        z_to=domcfg_to.gdepw_0.isel({"x_c": 1, "y_c": 1}).drop_vars(["x_c", "y_c"]),
+        scale_factor_fr=ds.e3u_0,
+        scale_factor_to=ds.e3t_1d,
+        z_fr=grid_fr.interp(ds.gdepw_0, "X", boundary="extend"),
+        z_to=ds.gdepw_1d,
     )
-    _assert_same_integrated_value(
-        v_fr, v_to, e3_fr=domcfg_fr.e3u_0, e3_to=domcfg_to.e3u_0
-    )
+    _assert_same_integrated_value(v_fr, v_to, e3_fr=ds.e3u_0, e3_to=ds.e3t_1d)
 
 
 def test_U_1():
-    domcfg_fr = open_domcfg_fr()
-    nemo_ds = xr.open_dataset(TESTPATH / "data/xnemogcm.nemo.nc")
-    nemo_ds.load()
-    domcfg_to = open_domcfg_to()
+    ds = open_ds()
 
-    grid_fr = xgcm.Grid(domcfg_fr, periodic=False)
-    grid_to = xgcm.Grid(domcfg_to, periodic=False)
+    grid_fr = xgcm.Grid(ds, periodic=False)
+    grid_to = xgcm.Grid(ds, periodic=False)
 
-    v_fr = nemo_ds["uo"] * 1
+    v_fr = (ds["uo"] * 0 + 1) * ds.umask
     v_to = remap_vertical(
         v_fr,
         grid_fr,
         grid_to,
         axis="Z",
-        scale_factor_fr=domcfg_fr.e3u_0,
-        scale_factor_to=domcfg_to.e3u_0,
-        z_fr=grid_fr.interp(domcfg_fr.gdepw_0, "X", boundary="extend"),
-        z_to=domcfg_to.gdepw_0.isel({"x_c": 1, "y_c": 1}).drop_vars(["x_c", "y_c"]),
+        scale_factor_fr=ds.e3u_0,
+        scale_factor_to=ds.e3t_1d,
+        z_fr=grid_fr.interp(ds.gdepw_0, "X", boundary="extend"),
+        z_to=ds.gdepw_1d,
     )
-    _assert_same_integrated_value(
-        v_fr, v_to, e3_fr=domcfg_fr.e3u_0, e3_to=domcfg_to.e3u_0
-    )
+    _assert_same_integrated_value(v_fr, v_to, e3_fr=ds.e3u_0, e3_to=ds.e3t_1d)
 
 
 def test_U():
-    domcfg_fr = open_domcfg_fr()
-    nemo_ds = xr.open_dataset(TESTPATH / "data/xnemogcm.nemo.nc")
-    nemo_ds.load()
-    domcfg_to = open_domcfg_to()
+    ds = open_ds()
 
-    grid_fr = xgcm.Grid(domcfg_fr, periodic=False)
-    grid_to = xgcm.Grid(domcfg_to, periodic=False)
+    grid_fr = xgcm.Grid(ds, periodic=False)
+    grid_to = xgcm.Grid(ds, periodic=False)
 
-    v_fr = nemo_ds["uo"]
+    v_fr = ds["uo"]
     v_to = remap_vertical(
         v_fr,
         grid_fr,
         grid_to,
         axis="Z",
-        scale_factor_fr=domcfg_fr.e3u_0,
-        scale_factor_to=domcfg_to.e3u_0,
-        z_fr=grid_fr.interp(domcfg_fr.gdepw_0, "X", boundary="extend"),
-        z_to=domcfg_to.gdepw_0.isel({"x_c": 1, "y_c": 1}).drop_vars(["x_c", "y_c"]),
+        scale_factor_fr=ds.e3u_0,
+        scale_factor_to=ds.e3t_1d,
+        z_fr=grid_fr.interp(ds.gdepw_0, "X", boundary="extend"),
+        z_to=ds.gdepw_1d,
     )
-    _assert_same_integrated_value(
-        v_fr, v_to, e3_fr=domcfg_fr.e3u_0, e3_to=domcfg_to.e3u_0
-    )
+    _assert_same_integrated_value(v_fr, v_to, e3_fr=ds.e3u_0, e3_to=ds.e3t_1d)
 
 
 def test_W():
-    domcfg_fr = open_domcfg_fr()
-    nemo_ds = xr.open_dataset(TESTPATH / "data/xnemogcm.nemo.nc")
-    nemo_ds.load()
-    domcfg_to = open_domcfg_to()
+    ds = open_ds()
 
-    grid_fr = xgcm.Grid(domcfg_fr, periodic=False, metrics=_metrics)
-    grid_to = xgcm.Grid(domcfg_to, periodic=False, metrics=_metrics_to)
+    grid_fr = xgcm.Grid(ds, periodic=False, metrics=metrics_fr)
+    grid_to = xgcm.Grid(ds, periodic=False, metrics=metrics_to)
 
-    v_fr = nemo_ds["woce"]
+    v_fr = ds["woce"]
     v_to = remap_vertical(v_fr, grid_fr, grid_to, axis="Z",)
-    _assert_same_integrated_value(
-        v_fr, v_to, e3_fr=domcfg_fr.e3w_0, e3_to=domcfg_to.e3w_1d
-    )
+    _assert_same_integrated_value(v_fr, v_to, e3_fr=ds.e3w, e3_to=ds.e3w_1d)
 
 
 def test_W_1():
-    domcfg_fr = open_domcfg_fr()
-    nemo_ds = xr.open_dataset(TESTPATH / "data/xnemogcm.nemo.nc")
-    nemo_ds.load()
-    domcfg_to = open_domcfg_to()
+    ds = open_ds()
 
-    grid_fr = xgcm.Grid(domcfg_fr, periodic=False, metrics=_metrics)
-    grid_to = xgcm.Grid(domcfg_to, periodic=False, metrics=_metrics_to)
+    grid_fr = xgcm.Grid(ds, periodic=False, metrics=metrics_fr)
+    grid_to = xgcm.Grid(ds, periodic=False, metrics=metrics_to)
 
-    v_fr = nemo_ds["woce"] * 0 + 1
+    v_fr = ds["woce"] * 0 + 1
     v_to = remap_vertical(v_fr, grid_fr, grid_to, axis="Z",)
-    _assert_same_integrated_value(
-        v_fr, v_to, e3_fr=domcfg_fr.e3w_0, e3_to=domcfg_to.e3w_1d
-    )
+    _assert_same_integrated_value(v_fr, v_to, e3_fr=ds.e3w, e3_to=ds.e3w_1d)
 
 
 def test_T_1_auto_get_scale_factor_to():
-    domcfg_fr = open_domcfg_fr()
-    nemo_ds = xr.open_dataset(TESTPATH / "data/xnemogcm.nemo.nc")
-    nemo_ds.load()
-    domcfg_to = open_domcfg_to()
+    ds = open_ds()
 
-    grid_fr = xgcm.Grid(domcfg_fr, periodic=False)
-    grid_to = xgcm.Grid(domcfg_to, periodic=False, metrics=_metrics)
+    grid_fr = xgcm.Grid(ds, periodic=False)
+    grid_to = xgcm.Grid(ds, periodic=False, metrics=metrics_to)
 
-    v_fr = (nemo_ds["thetao"] * 0 + 1) * domcfg_fr.tmask
-    v_to = remap_vertical(
-        v_fr, grid_fr, grid_to, axis="Z", scale_factor_fr=domcfg_fr.e3t_0
-    )
-    _assert_same_integrated_value(
-        v_fr, v_to, e3_fr=domcfg_fr.e3t_0, e3_to=domcfg_to.e3t_0
-    )
+    v_fr = (ds["thetao"] * 0 + 1) * ds.tmask
+    v_to = remap_vertical(v_fr, grid_fr, grid_to, axis="Z", scale_factor_fr=ds.e3t)
+    _assert_same_integrated_value(v_fr, v_to, e3_fr=ds.e3t, e3_to=ds.e3t_1d)
 
 
 def test_T_1():
-    domcfg_fr = open_domcfg_fr()
-    nemo_ds = xr.open_dataset(TESTPATH / "data/xnemogcm.nemo.nc")
-    nemo_ds.load()
-    domcfg_to = open_domcfg_to()
+    ds = open_ds()
 
-    grid_fr = xgcm.Grid(domcfg_fr, periodic=False)
-    grid_to = xgcm.Grid(domcfg_to, periodic=False)
+    grid_fr = xgcm.Grid(ds, periodic=False)
+    grid_to = xgcm.Grid(ds, periodic=False)
 
-    v_fr = (nemo_ds["thetao"] * 0 + 1) * domcfg_fr.tmask
+    v_fr = (ds["thetao"] * 0 + 1) * ds.tmask
     v_to = remap_vertical(
         v_fr,
         grid_fr,
         grid_to,
         axis="Z",
-        scale_factor_fr=domcfg_fr.e3t_0,
-        scale_factor_to=domcfg_to.e3t_0,
+        scale_factor_fr=ds.e3t,
+        scale_factor_to=ds.e3t_1d,
     )
-    _assert_same_integrated_value(
-        v_fr, v_to, e3_fr=domcfg_fr.e3t_0, e3_to=domcfg_to.e3t_0
-    )
+    _assert_same_integrated_value(v_fr, v_to, e3_fr=ds.e3t, e3_to=ds.e3t_1d)
 
 
 def test_T_theta():
-    domcfg_fr = open_domcfg_fr()
-    nemo_ds = xr.open_dataset(TESTPATH / "data/xnemogcm.nemo.nc")
-    nemo_ds.load()
-    domcfg_to = open_domcfg_to()
+    ds = open_ds()
 
-    grid_fr = xgcm.Grid(domcfg_fr, periodic=False)
-    grid_to = xgcm.Grid(domcfg_to, periodic=False)
+    grid_fr = xgcm.Grid(ds, periodic=False)
+    grid_to = xgcm.Grid(ds, periodic=False)
 
-    v_fr = nemo_ds["thetao"] * domcfg_fr.tmask
+    v_fr = ds["thetao"] * ds.tmask
     v_to = remap_vertical(
         v_fr,
         grid_fr,
         grid_to,
         axis="Z",
-        scale_factor_fr=domcfg_fr.e3t_0,
-        scale_factor_to=domcfg_to.e3t_0,
+        scale_factor_fr=ds.e3t,
+        scale_factor_to=ds.e3t_1d,
     )
-    _assert_same_integrated_value(
-        v_fr, v_to, e3_fr=domcfg_fr.e3t_0, e3_to=domcfg_to.e3t_0
-    )
+    _assert_same_integrated_value(v_fr, v_to, e3_fr=ds.e3t, e3_to=ds.e3t_1d)
 
 
 def test_T_theta_full_automatic():
-    domcfg_fr = open_domcfg_fr()
-    nemo_ds = xr.open_dataset(TESTPATH / "data/xnemogcm.nemo.nc")
-    nemo_ds.load()
-    domcfg_to = open_domcfg_to()
+    ds = open_ds()
 
-    grid_fr = xgcm.Grid(domcfg_fr, periodic=False, metrics=_metrics)
-    grid_to = xgcm.Grid(domcfg_to, periodic=False, metrics=_metrics)
+    grid_fr = xgcm.Grid(ds, periodic=False, metrics=metrics_fr)
+    grid_to = xgcm.Grid(ds, periodic=False, metrics=metrics_to)
 
-    v_fr = nemo_ds["thetao"] * domcfg_fr.tmask
-    v_to = remap_vertical(
-        v_fr,
-        grid_fr,
-        grid_to,
-        axis="Z",
-        scale_factor_fr=domcfg_fr.e3t_0,
-        scale_factor_to=domcfg_to.e3t_0,
-    )
+    v_fr = ds["thetao"] * ds.tmask
+    v_to = remap_vertical(v_fr, grid_fr, grid_to, axis="Z",)
     v_to_auto = remap_vertical(v_fr, grid_fr, grid_to, axis="Z")
-    _assert_same_integrated_value(
-        v_fr, v_to_auto, e3_fr=domcfg_fr.e3t_0, e3_to=domcfg_to.e3t_0
-    )
+    _assert_same_integrated_value(v_fr, v_to_auto, e3_fr=ds.e3t, e3_to=ds.e3t_1d)
     _assert_same_domcfg(v_to, v_to_auto)
 
 
 def test_T_1_same_fr_and_to():
-    domcfg_fr = open_domcfg_fr()
+    ds = open_ds()
 
-    nemo_ds = xr.open_dataset(TESTPATH / "data/xnemogcm.nemo.nc")
-    nemo_ds.load()
-    domcfg_to = domcfg_fr
+    grid_fr = xgcm.Grid(ds, periodic=False)
+    grid_to = xgcm.Grid(ds, periodic=False)
 
-    grid_fr = xgcm.Grid(domcfg_fr, periodic=False)
-    grid_to = xgcm.Grid(domcfg_to, periodic=False)
-
-    v_fr = (nemo_ds["thetao"] * 0 + 1) * domcfg_fr.tmask
+    v_fr = (ds["thetao"] * 0 + 1) * ds.tmask
     v_to = remap_vertical(
         v_fr,
         grid_fr,
         grid_to,
         axis="Z",
-        scale_factor_fr=domcfg_fr.e3t_0,
-        scale_factor_to=domcfg_to.e3t_0,
+        scale_factor_fr=ds.e3t_0,
+        scale_factor_to=ds.e3t_0,
     )
     _assert_same_domcfg(v_fr, v_to)
 
 
 def test_T_theta_same_fr_and_to():
-    domcfg_fr = open_domcfg_fr()
+    ds = open_ds()
 
-    nemo_ds = xr.open_dataset(TESTPATH / "data/xnemogcm.nemo.nc")
-    nemo_ds.load()
-    domcfg_to = domcfg_fr
+    grid_fr = xgcm.Grid(ds, periodic=False)
+    grid_to = xgcm.Grid(ds, periodic=False)
 
-    grid_fr = xgcm.Grid(domcfg_fr, periodic=False)
-    grid_to = xgcm.Grid(domcfg_to, periodic=False)
-
-    v_fr = nemo_ds["thetao"] * domcfg_fr.tmask
-    v_fr.data = v_fr.data.astype(np.float16)
-    v_fr.data = v_fr.data.astype(np.float128)
+    v_fr = ds["thetao"] * ds.tmask
     v_to = remap_vertical(
         v_fr,
         grid_fr,
         grid_to,
         axis="Z",
-        scale_factor_fr=domcfg_fr.e3t_0,
-        scale_factor_to=domcfg_to.e3t_0,
+        scale_factor_fr=ds.e3t_0,
+        scale_factor_to=ds.e3t_0,
     )
 
     _assert_same_domcfg(v_fr, v_to)
