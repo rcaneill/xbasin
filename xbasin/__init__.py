4c4672504a07fcaa085a748745a455a2f9574512
Romain <romain.caneill@yahoo.fr>
2021-02-02 14:54:21 +0100

    removing vertical interpolation functions

--- xbasin/__init__.py
+++ xbasin/__init__.py
@@ -1,4 +1,3 @@
-from ._remapping import remap_vertical
 from ._stream_functions import (
     compute_moc,
     create_density_grid,
---
+++ xbasin/tests/test_import.py
@@ -0,0 +1,8 @@
+import pytest
+
+
+def test_import_all():
+    """
+    Test the importation of xbasin
+    """
+    import xbasin
