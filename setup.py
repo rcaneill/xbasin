82a3c66f5b3ee4251c098ac849772d04d1f17d74
Romain <romain.caneill@yahoo.fr>
2021-02-09 11:55:00 +0100

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup, find_packages, dist
 
 DISTNAME = "xbasin"
-VERSION = "0.0.3"
+VERSION = "0.0.4"
 LICENSE = "MIT"
 AUTHOR = "Romain Caneill"
 AUTHOR_EMAIL = "romain.caneill@gu.se"
